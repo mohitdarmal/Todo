97c56fb6edfa021b1ffd5f92d468d26a91fd9b40
mohit darmal <mohitdarmal1907@gmail.com>
2019-03-10 22:19:39 +0530

    updates

--- public/js/todo.js
+++ public/js/todo.js
@@ -18,7 +18,23 @@ title.forEach((todo) => {
 })
 
 var timeAlert = document.querySelectorAll('.time-alert');
+var dataHeading = document.querySelector('#data-heading');
+var container = document.querySelector('.container');
 
+var row = document.querySelectorAll('.row-bg');
+for(var i = 0; i < row.length; i++){
+    console.log(row[i]);
+    if((row[i])){
+        // dataHeading.style.display = 'block'
+        console.log(true)
+        
+    }
+    else{
+       /*  dataHeading.style.display = 'none'
+        console.log(false) */
+        
+    }
+}
 /* var date = new Date();
 var hr = date.getHours();
 var min = date.getMinutes();
